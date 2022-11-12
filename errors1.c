1d7f25def2ab977616b797e0f9ae5a2a65179fab
osagiede adams osahon <111079063+justosasxxp@users.noreply.github.com>
2022-11-12 11:33:50 +0100

    commit

--- errors1.c
+++ errors1.c
@@ -50,7 +50,7 @@ void print_error(info_t *info, char *estr)
  * print_d - function prints a decimal (integer) number (base 10)
  * @input: the input
  * @fd: the filedescriptor to write to
- * this is the description for a file
+ * 
  * Return: number of characters printed
  */
 int print_d(int input, int fd)
